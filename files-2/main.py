78ca036d7ef37b01d4388559cf3f8a8d05982fca
Taaruni Ananya <106027676+tanya-ananya@users.noreply.github.com>
2024-10-23 12:13:57 -0500

    Add files via upload

---
+++ files-2/main.py
@@ -0,0 +1,18 @@
+class Stack:
+    def __init__(self):
+        self._data = [] # nonpublic list instance   
+    def __len__(self):
+        return len(self._data)
+    def is_empty(self):
+        return len(self._data) == 0
+    def push(self, e):
+        self._data.append(e) # new item stored at end of list
+    def top(self):
+        if self.is_empty():
+            raise Empty('Stack is empty')
+        return self._data[-1] # the last item in the list
+    def pop(self):
+        if self.is_empty():
+            raise Empty('Stack is empty')
+        return self._data.pop() # remove last item from list
+
