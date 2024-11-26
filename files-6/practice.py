2b12c304b0e575361db0ffa81915608054bffca3
Taaruni Ananya <106027676+tanya-ananya@users.noreply.github.com>
2024-11-26 11:43:20 -0600

    Add files via upload

---
+++ files-6/practice.py
@@ -0,0 +1,14 @@
+import numpy as np
+import random
+
+print(np.zeros((3,3)))
+print(np.arange(0, 20, 3))
+print(np.random.randint(0, 20, (4, 4)))
+
+'''arr = np.array([[1, 2], [2, 3]])
+print()
+print()
+print(type(arr))
+print(arr.shape)
+print(arr)
+print()'''
