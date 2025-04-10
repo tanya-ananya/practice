c6027eff45cc5b50b096df0f5cb2ec96ff57c447
Taaruni Ananya <106027676+tanya-ananya@users.noreply.github.com>
2025-04-10 18:54:45 -0400

    Add files via upload

--- files-8/practice.py
+++ files-8/practice.py
@@ -1,55 +1,20 @@
-def minHeapify(arr, n, i):
-    smallest = i
-    left = 2 * i + 1
-    right = 2 * i + 2
+import heapq
 
-    if left < n and arr[left] < arr[smallest]:
-        smallest = left
-    if right < n and arr[right] < arr[smallest]:
-        smallest = right
+class PriorityQueue:
+    def __init__(self):
+        self.heap = []
 
-    if smallest != i:
-        arr[i], arr[smallest] = arr[smallest], arr[i]
-        minHeapify(arr, n, smallest)
-
-def maxHeapify(arr, n, i):
-    largest = i
-    left = 2 * i + 1
-    right = 2 * i + 2
-
-    if left < n and arr[left] > arr[largest]:
-        largest = left
-    if right < n and arr[right] > arr[largest]:
-        largest = right
-
-    if largest != i:
-        arr[i], arr[largest] = arr[largest], arr[i]
-        maxHeapify(arr, n, largest)
-
-def minSort(arr):
-    n = len(arr)
-
-    if n <= 1:
-        return arr
-
-    for x in range(n//2 - 1, -1, -1):
-        minHeapify(arr, n, x)
-
-    for x in range(n - 1, 0, -1):
-        arr[x], arr[0] = arr[0], arr[x]
-        minHeapify(arr, x, 0)
-    return arr
-
-def maxSort(arr):
-    n = len(arr)
-
-    if n <= 1:
-        return arr
-
-    for x in range(n//2 - 1, -1, -1):
-        maxHeapify(arr, n, x)
-
-    for x in range(n - 1, 0, -1):
-        arr[x], arr[0] = arr[0], arr[x]
-        maxHeapify(arr, x, 0)
-    return arr
+    def insert(self, item):
+        heapq.heappush(self.heap, item)
+    def peek(self):
+        if not self.heap:
+            return self.heap[0]
+        return None
+    def extract(self):
+        if not self.is_empty():
+            return heapq.heappop(self.heap)
+        return None
+    def is_empty(self):
+        return len(self.heap) == 0
+    def length(self):
+        return len(self.heap)
